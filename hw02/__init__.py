b57087bb5211bbda454c1a697c9392e359c0aac0
Thomas Fike <fike@me.com>
2020-04-26 14:49:57 -0700

    Added new hw02

--- hw02/__init__.py
+++ hw02/__init__.py
@@ -14,3 +14,8 @@ def compiles():
 def test_set_1():
     """test set 1"""
     check50.run("gcc hw02.c; ./a.out").stdin("1\n2\nq\n", prompt=False).stdout("You entered 2 integers.\n2 of the integers were positive.\n0 of the integers were negative.\nThe sum of the 2 integers was 3.\n", regex=False).exit(0)
+
+@check50.check()
+def test_set_2():
+    """test set 2"""
+    check50.run("./a.out").stdin("q", prompt=False).exit(0)
