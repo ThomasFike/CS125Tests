9702e96182f35625a107856643d05e7c48c71021
Thomas Fike <fike@me.com>
2020-04-26 17:21:53 -0700

    New hw02

--- hw02new/__init__.py
+++ hw02new/__init__.py
@@ -17,6 +17,16 @@ def test_set_1():
     check50.run("./hw02").stdin("1\n2\nq\n", prompt=False).stdout("You entered 2 integers.\n2 of the integers were positive.\n0 of the integers were negative.\nThe sum of the 2 integers was 3.\n", regex=False).exit(0)
 
 @check50.check(compiles)
-def test_set_2():
-    """test set 2"""
+def no_data():
+    """q is used with no data entered"""
     check50.run("./hw02").stdin("q", prompt=False).exit(0)
+
+@check50.check(compiles)
+def large_data_set():
+    """Very Large DataSet"""
+    check50.run("./hw02").stdin("5884\n937\n-3532\n7223\n-7922\nq\n", prompt=True).stdout("You entered 5 integers.\n3 of the integers were positive.\n2 of the integers were negative.\nThe sum of the 5 integers was 2590.\n", regex=False).exit(0)
+
+@check50.check(compiles)
+def one_value():
+    """one value"""
+    check50.run("./hw02").stdin("256\nq\n", prompt=True).stdout("You entered 1 integers.\n1 of the integers were positive.\n0 of the integers were negative.\nThe sum of the 1 integers was 256.\n", regex=False).exit(0)
